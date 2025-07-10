ad1852af7aaff35e09af5e12938d9a7d7fc0bab1
SistemaRayoXP <edsonarmando78@outlook.com>
2025-07-10 00:42:27 -0600

    Updated repo generation logic

--- gen_repo.sh
+++ gen_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config user.name "[CI] Alfa"
-git config user.email "alfa-add-on@users.noreply.github.com"
+git config user.name "${GIT_USER_NAME:='Alfa Development Group'}"
+git config user.email "${GIT_USER_EMAIL:='145614550+alfa-addon@users.noreply.github.com'}"
 
 BLACKLIST="downloads mediaserver"
 EXCLUDES=""
@@ -15,15 +15,9 @@ ADDON_ZIPS=$(eval "find . -maxdepth 2 -type f -name '*.zip' $EXCLUDES")
 echo "The following zips were found:"
 echo $ADDON_ZIPS
 
-echo "Installing Python dependencies"
-
-pip install lxml
-
 echo "Updating repository"
-
 python create_repository.py $ADDON_ZIPS
 git add .
 git commit -m "Updated addons.xml and md5's"
 git push
-
 echo "Update finished!"
